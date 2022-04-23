eebb94ea747fdb66dca39149a1997f60b2568b7c
Michael Zappe <zapman@z-meta.com>
2022-04-23 00:26:55 -0600

    Now with all the buffers and trigger unit

--- build.tcl
+++ build.tcl
@@ -1,11 +1,11 @@
-cd /Users/zapma/Pi-Radio/SDR2-HDL
-
 open_project SDR2-HDL.xpr
 set_property XPM_LIBRARIES {XPM_CDC XPM_MEMORY XPM_FIFO} [current_project]
 update_compile_order -fileset sources_1
 open_bd_design {SDR2-HDL.srcs/sources_1/bd/SDRv2/SDRv2.bd}
 
 source update.tcl
+
+synth_design -top SDRv2_wrapper -part xczu28dr-ffvg1517-2-e -verbose
 
 #reset_run synth_1
 #launch_runs synth_1 -jobs 2
--- update.tcl
+++ update.tcl
@@ -1,12 +1,11 @@
-cd /Users/zapma/Documents/PiRadIP
-source generate_all.tcl
-
-cd /Users/zapma/Pi-Radio/SDR2-HDL
-update_ip_catalog -rebuild -repo_path c:/Users/zapma/Documents/PiRadIP
+update_ip_catalog -rebuild -repo_path ../PiRadIP
 report_ip_status
-upgrade_ip [get_ips  {SDRv2_axis_sample_buffer_o_0_10 SDRv2_axis_sample_buffer_o_0_11 SDRv2_axis_sample_buffer_o_0_14 SDRv2_axis_sample_buffer_o_0_17 SDRv2_axis_sample_buffer_o_0_15 SDRv2_axis_sample_buffer_o_0_13 SDRv2_axis_sample_buffer_o_0_12 SDRv2_axis_sample_buffer_o_0_16 SDRv2_piradspi_ip_0_0}] -log ip_upgrade.log
-export_ip_user_files -of_objects [get_ips {SDRv2_axis_sample_buffer_o_0_10 SDRv2_axis_sample_buffer_o_0_11 SDRv2_axis_sample_buffer_o_0_14 SDRv2_axis_sample_buffer_o_0_17 SDRv2_axis_sample_buffer_o_0_15 SDRv2_axis_sample_buffer_o_0_13 SDRv2_axis_sample_buffer_o_0_12 SDRv2_axis_sample_buffer_o_0_16 SDRv2_piradspi_ip_0_0}] -no_script -sync -force 
-generate_target all [get_files  C:/Users/zapma/Pi-Radio/SDR2-HDL/SDR2-HDL.srcs/sources_1/bd/SDRv2/SDRv2.bd]
+upgrade_ip [get_ips  {SDRv2_axis_sample_buffer_o_0_0 SDRv2_axis_sample_buffer_o_1_0 SDRv2_axis_sample_buffer_o_2_0 SDRv2_axis_sample_buffer_o_3_0 SDRv2_axis_sample_buffer_o_4_0 SDRv2_axis_sample_buffer_o_5_0 SDRv2_axis_sample_buffer_o_6_0 SDRv2_axis_sample_buffer_o_7_0 SDRv2_piradspi_ip_0_0}] -log ip_upgrade.log
+export_ip_user_files -of_objects [get_ips {SDRv2_axis_sample_buffer_o_0_10 SDRv2_axis_sample_buffer_o_0_11 SDRv2_axis_sample_buffer_o_0_14 SDRv2_axis_sample_buffer_o_0_17 SDRv2_axis_sample_buffer_o_0_15 SDRv2_axis_sample_buffer_o_0_13 SDRv2_axis_sample_buffer_o_0_12 SDRv2_axis_sample_buffer_o_0_16 SDRv2_piradspi_ip_0_0}] -no_script -sync -force \
+    
+generate_target all [get_files  SDR2-HDL.srcs/sources_1/bd/SDRv2/SDRv2.bd]
+
+export_ip_user_files -of_objects [get_files SDR2-HDL.srcs/sources_1/bd/SDRv2/SDRv2.bd] -no_script -sync -force -quiet
+
 report_ip_status
 
-synth_design -top SDRv2_wrapper -part xczu28dr-ffvg1517-2-e -verbose
